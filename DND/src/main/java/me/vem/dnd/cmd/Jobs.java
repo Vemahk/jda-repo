1ba2d026155c4caa41c4a33fbc6323ee3806bc54
Vemahk <Vemahk20@gmail.com>
2018-11-04 23:08:23 -0600

    DND Job Rework
    Cleaned up the Jobs command code. It was a bit messy and not made to be compatible with multiple guilds, so it done been cleaned and fixed.

--- DND/src/main/java/me/vem/dnd/cmd/Jobs.java
+++ DND/src/main/java/me/vem/dnd/cmd/Jobs.java
@@ -3,8 +3,9 @@ package me.vem.dnd.cmd;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +18,7 @@ import me.vem.jdab.utils.ExtFileManager;
 import me.vem.jdab.utils.Logger;
 import me.vem.jdab.utils.Respond;
 import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
@@ -29,7 +31,7 @@ public class Jobs extends Command implements Configurable{
 		if(instance == null) instance = new Jobs();
 	}
 	
-	private Map<String, List<Job>> jobsDatabase;
+	private Map<Long, Map<String, List<Job>>> database;
 	
 	private Jobs() {
 		super("jobs");
@@ -42,152 +44,145 @@ public class Jobs extends Command implements Configurable{
 		
 		TextChannel channel = event.getChannel();
 		Message userMsg = event.getMessage();
+		Guild guild = event.getGuild();
 		
-		if(args.length == 0) {
-			Respond.timeout(channel, userMsg, 10000, help());
-			return true;
-		}
+		if(args.length == 0)
+			return getHelp(channel, userMsg, 10000);
 		
-		String city = args[0];
-		if(city.equals("add")) {
-			if(args.length < 4) {
-				Respond.timeout(channel, userMsg, 5000, help());
+		Map<String, List<Job>> gDatabase = database.get(guild.getIdLong());
+		if(gDatabase == null)
+			database.put(guild.getIdLong(), gDatabase = new LinkedHashMap<>());
+		
+		if("cities".equals(args[0])) {
+			if(gDatabase.size() != 0) {
+				StringBuilder response = new StringBuilder("Known Cities:\n```\n");
+				for(String s : gDatabase.keySet())
+					response.append(s).append('\n');
+				Respond.timeout(channel, userMsg, 7500, response.append("```").toString());
+			}else Respond.timeout(channel, userMsg, 5000, "Your guild does not have any cities with job listings.");
+		}else if("jobkeys".equals(args[0])) {
+			if(args.length < 2)
+				return !getHelp(channel, userMsg, 10000);
+			
+			args[1] = casify(args[1]);
+			
+			List<Job> jobs = gDatabase.get(args[1]);
+			if(jobs == null) {
+				Respond.timeoutf(channel, userMsg, 5000, "`%s` is not a known city.", args[1]);
 				return false;
 			}
 			
-			String trueCity = casify(args[1]);
-			String key = args[2];
-			String desc = "";
-			for(int i=3;i<args.length;)
-				desc += args[i] + (++i<args.length ? " " : "");
+			StringBuilder response = new StringBuilder("Keys of Jobs in ").append(args[1]).append(":\n```\n");
+			for(Job job : jobs)
+				response.append(job.key).append('\n');
+			Respond.timeout(channel, userMsg, 10000, response.append("```").toString());
+		}else if("add".equals(args[0])){
+			if(args.length < 4)
+				return !getHelp(channel, userMsg, 10000);
 			
-			Job j = new Job(trueCity, desc, key);
-			addJob(trueCity, j);
+			args[1] = casify(args[1]);
+			List<Job> jobs = gDatabase.get(args[1]);
+			if(jobs == null)
+				gDatabase.put(args[1], jobs = new LinkedList<>());
 			
-			Respond.timeout(channel, userMsg, 5000, "Job added to "+trueCity+"!");
-			
-		}else if(city.equals("remove")) {
-			
-			if(args.length < 3) {
-				Respond.timeout(channel, userMsg, 5000, help());
-				return false;
-			}
-			
-			String trueCity = casify(args[1]);
-			String key = args[2];
-			
-			if(jobsDatabase.containsKey(trueCity)) {
-				List<Job> jobs = jobsDatabase.get(trueCity);
-				Job target = null;
-				for(Job j : jobs) {
-					if(j.getKey().equals(key)) {
-						target = j;
-						break;
-					}
-				}
-				
-				if(target==null) Respond.timeout(channel, userMsg, 5000, "Job not found by key "+key);
-				else{
-					jobs.remove(target);
-					if(jobs.size() == 0) jobsDatabase.remove(trueCity);
-					Respond.timeout(channel, userMsg, 5000, "Job '"+key+"' removed!");
-				}
-			}else Respond.timeout(channel, userMsg, 5000, "No such city.");
-		
-		}else if(city.equals("cities")){
-			String rsp = "";
-			for(String s : jobsDatabase.keySet()) rsp+=s+"\n";
-			Respond.timeout(channel, userMsg, 10000, "Known Cities:\n"+rsp);
-		}else{
-			city = casify(city);
-			
-			if(args.length > 1 && args[1].equals("getkeys"))
-				if(event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
-					if(jobsDatabase.containsKey(city)) {
-						String rsp = "";
-						for(Job j : jobsDatabase.get(city)) rsp += j.getKey()+"\n";
-						Respond.timeout(channel, userMsg, 10000, city+" Job Keys: \n"+rsp);
-						return true;
-					}else{
-						Respond.timeout(channel, userMsg, 5000, "No such city.");
-						return true;
-					}
-				}else{
-					Respond.timeout(channel, userMsg, 5000, "Only the GM can get the keys of the jobs.");
+			Job j = new Job(args[2], args[3]);
+			for(Job job : jobs)
+				if(job.equals(j)) {
+					Respond.timeoutf(channel, userMsg, 5000, "There's already a job with the key `%s`.", args[2]);
 					return false;
 				}
 			
-			if(jobsDatabase.containsKey(city)) {
-				String rsp = "";
-				for(Job j : jobsDatabase.get(city)) rsp += j.getDesc() + "\n\n";
-				Respond.timeout(channel, userMsg, 60000, "Jobs at "+city+":\n"+rsp+"(Note: This message will be removed in 1 minute)");
-			}else Respond.timeout(channel, userMsg, 5000, city + " does not appear to have any job listings at this moment. Try again later when the GM isn't a lazy bum.");
+			jobs.add(j);
+			Respond.timeoutf(channel, userMsg, 5000, "Job `%s` added to the city `%s`", args[2], args[1]);
+		}else if("remove".equals(args[0])){
+			if(args.length < 3)
+				return !getHelp(channel, userMsg, 10000);
+			
+			args[1] = casify(args[1]);
+			
+			List<Job> jobs = gDatabase.get(args[1]);
+			if(jobs == null) {
+				Respond.timeoutf(channel, userMsg, 5000, "The city `%s` has no job listings.", args[1]);
+				return false;
+			}
+			
+			boolean found = false;
+			
+			Iterator<Job> iter = jobs.iterator();
+			while(iter.hasNext()) {
+				Job cur = iter.next();
+				if(cur.key.equals(args[2])) {
+					iter.remove();
+					found = true;
+					break;
+				}
+			}
+			
+			if(found) Respond.timeoutf(channel, userMsg, 5000, "Job `%s` removed from the posting in city `%s`.", args[2], args[1]);
+			else Respond.timeoutf(channel, userMsg, 5000, "`%s` is not the key of a listed job in `%s`.", args[2], args[1]);
+		}else {
+			args[0] = casify(args[0]);
+			List<Job> jobs = gDatabase.get(args[0]);
+			if(jobs == null) {
+				Respond.timeoutf(channel, userMsg, 5000, "`%s` does not have any job listings.", args[0]);
+				return false;
+			}
+			
+			StringBuilder response = new StringBuilder("Jobs in ").append(args[0]).append(":\n```\n");
+			int i=1;
+			for(Job job : jobs) 
+				response.append(i++).append(") ").append(job.desc).append("\n\n");
+			
+			Respond.timeoutf(channel, userMsg, 60000, response.append("```").toString());
 		}
+		
 		return true;
 	}
 
+	@Override protected String help() {
+		return "Usage:\n```\n"
+			 + "jobs <city> -- Prints the list of jobs in that city.\n"
+			 + "jobs cities -- Prints a list of cities.\n"
+			 + "jobs jobkeys <city> -- Prints the keys of the jobs in that city.\n"
+			 + "jobs add <city> <job-key> <description> -- Adds a job to a city.\n"
+			 + "jobs remove <city> <job-key> -- Removes a job from a city.\n"
+			 + "```";
+	}
+	
+	public boolean getHelp(TextChannel channel, Message userMsg, long delay) {
+		Respond.timeout(channel, userMsg, delay, help());
+		return true;
+	}
+	
 	@Override public boolean hasPermissions(GuildMessageReceivedEvent event, String... args) {
 		if(args.length > 0 && ("add".equals(args[0]) || "remove".equals(args[0])))
 			return event.getMember().hasPermission(Permission.ADMINISTRATOR);
 		return true;
 	}
-
-	private boolean addJob(String city, Job j) {
-		if(jobsDatabase.containsKey(city)) {
-			List<Job> jobs = jobsDatabase.get(city);
-			for(Job job : jobs) 
-				if(job.getKey().equals(j.getKey()))
-					return false;
-			jobs.add(j);
-		}
-		else {
-			ArrayList<Job> l = new ArrayList<>();
-			l.add(j);
-			jobsDatabase.put(city, l);
-		}
-		Logger.infof("Job '%s' added!", j.getKey());
-		return true;
-	}
 	
 	private String casify(String s) {
-		s = s.toLowerCase();
-		return Character.toUpperCase(s.charAt(0)) + s.substring(1);
+		return Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase();
 	}
 	
 	class Job{
-		private String city;
-		private String desc;
-		private String key;
+		final String key, desc;
 		
-		public Job(String city, String desc, String key) {
-			this.city = city;
-			this.desc = desc;
+		public Job(String key, String desc) {
 			this.key = key;
+			this.desc = desc;
 		}
 		
-		public String getCity() { return city; }
-		public String getDesc() { return desc; }
-		public String getKey() { return key; }
-		
-		public String saveFormat() {
-			return city + ", " +key+": "+desc;
+		public boolean equals(Job other) {
+			if(other == null) return false;
+			return key.equals(other.key);
 		}
-	}
-
-	@Override protected String help() {
-		return "Usage:\n"
-			 + "jobs <cityname>\n"
-			 + "jobs <cityname> getkeys\n"
-			 + "jobs cities\n"
-			 + "jobs add <city> <job-key> <description>\n"
-			 + "jobs remove <city> <job-key>";
 	}
 
 	@Override
 	public void save() {
 		try {
 			PrintWriter out = ExtFileManager.getConfigOutput("jobs.json");
-			out.print(ExtFileManager.getGsonPretty().toJson(jobsDatabase));
+			out.print(ExtFileManager.getGsonPretty().toJson(database));
 			out.flush();
 			out.close();
 		} catch (IOException e) {
@@ -199,7 +194,7 @@ public class Jobs extends Command implements Configurable{
 
 	@Override
 	public void load() {
-		jobsDatabase = new HashMap<>();
+		database = new LinkedHashMap<>();
 		
 		File configFile = ExtFileManager.getConfigFile("jobs.json");
 		if(configFile == null) return;
@@ -208,7 +203,7 @@ public class Jobs extends Command implements Configurable{
 		if(content == null || content.length() == 0) return;
 		
 		Gson gson = ExtFileManager.getGsonPretty();
-		jobsDatabase = gson.fromJson(content, new TypeToken<HashMap<String, List<Job>>>(){}.getType());
+		database = gson.fromJson(content, new TypeToken<LinkedHashMap<Long, LinkedHashMap<String, LinkedList<Job>>>>(){}.getType());
 	}
 	
 	@Override
