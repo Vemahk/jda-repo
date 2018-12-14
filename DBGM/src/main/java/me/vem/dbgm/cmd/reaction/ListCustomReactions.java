f449f311a36537feef9e1d72c31853f484c48d4f
Vemahk <Vemahk20@gmail.com>
2018-12-14 09:38:59 -0600

    DBGM - LCR Menu
    Ported ListCustomReaction over to Foundation's menu system.

--- DBGM/src/main/java/me/vem/dbgm/cmd/reaction/ListCustomReactions.java
+++ DBGM/src/main/java/me/vem/dbgm/cmd/reaction/ListCustomReactions.java
@@ -1,9 +1,14 @@
 package me.vem.dbgm.cmd.reaction;
 
+import java.awt.Color;
+
 import me.vem.jdab.cmd.Command;
+import me.vem.jdab.struct.menu.EmbedMenu;
 import me.vem.jdab.utils.Respond;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageEmbed;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 public class ListCustomReactions extends Command{
@@ -21,49 +26,27 @@ public class ListCustomReactions extends Command{
 		if(!super.run(event, args))
 			return false;
 		
-		int page = 1;
-		if(args.length == 1) {
-			try {
-				page = Integer.parseInt(args[0]);
-			}catch(NumberFormatException e) {
-				return sendHelp(event.getChannel(), false);
-			}
-		}else if(args.length > 1)
-			return sendHelp(event.getChannel(), false);
-		
-		final int fPage = page; //For the lambda.
-		if(lastList == null) respondPage(event, page);
-		else {
-			long diff = System.currentTimeMillis() / 1000 - lastList.getCreationTime().toEpochSecond();
-			if(diff <= 60)
-				lastList.editMessage(getPage(event.getGuild(), page)).queue((msg_success) -> {/*We don't care.*/},
-					(error) -> respondPage(event, fPage));
-			
-			else respondPage(event, page);
-		}
+		new CRMenu(Respond.sync(event.getChannel(), getPage(event.getGuild(), 1)));
 		
 		return true;
 	}
-	
-	private Message lastList = null;
-	private void respondPage(GuildMessageReceivedEvent event, int page) {
-		if(lastList != null)
-			lastList.delete().queue((msg) -> {}, (err) -> {});
-		lastList = Respond.sync(event.getChannel(), getPage(event.getGuild(), page));
-	}
 
-	private String getPage(Guild guild, int page) {
+	private EmbedBuilder getPage(Guild guild, int page) {
+		EmbedBuilder builder = new EmbedBuilder().setColor(Color.RED);
+
 		String[] triggers = ReactionListener.getInstance().triggerList(guild);
-		if(triggers.length == 0) return "This guild has no Custom Reactions.";
+		if(triggers.length == 0) 
+			return builder.setTitle("This guild has no Custom Reactions.");
 		
-		if(triggers.length - 1 < (page-1) * 10) return "The Custom Reaction list does not have " + page + " pages";
+		builder.setTitle("Custom Reactions - Page " + page);
 		
-		StringBuilder rsp = new StringBuilder("[Custom Reaction List Page ").append(page).append("]```");
+		if(triggers.length - 1 < (page-1) * 10)
+			return builder.addField("No such page", "", false);
 		
 		for(int i=(page-1) * 10; i < triggers.length && i < page * 10;i++)
-			rsp.append('\n').append(triggers[i]);
+			builder.addField("CR #" + i, triggers[i], true);
 		
-		return rsp.append("```").toString();
+		return builder.setColor(Color.GREEN);
 	}
 	
 	@Override
@@ -74,7 +57,7 @@ public class ListCustomReactions extends Command{
 	@Override
 	public String[] usages() {
 		return new String[] {
-			"`lcr [pagenum=1]` -- Lists a given page of custom reactions."
+			"`lcr` -- Lists a given page of custom reactions."
 		};
 	}
 
@@ -86,5 +69,16 @@ public class ListCustomReactions extends Command{
 	@Override
 	public String getDescription() {
 		return "'lcr' is an initialization of List Custom Reactions.\nA custom reaction is a particular phrase that the bot will reply to with a specific message.";
+	}
+	
+	private class CRMenu extends EmbedMenu{
+		public CRMenu(Message msg) {
+			super(msg);
+		}
+
+		@Override
+		public MessageEmbed getEmbed(int page) {
+			return ListCustomReactions.this.getPage(msg.getGuild(), page).build();
+		}
 	}
 }
