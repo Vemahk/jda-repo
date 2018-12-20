64374ac0fbebcbefc2c618d688ea245a2e112bc4
Vemahk <Vemahk20@gmail.com>
2018-12-20 10:25:21 -0600

    Foundation - Pair.java
    A pair is just two associated items that aren't really key-value. I didn't want to use a map because it wouldn't accept duplicates.

---
+++ Foundation/src/main/java/me/vem/jdab/struct/Pair.java
@@ -0,0 +1,15 @@
+package me.vem.jdab.struct;
+
+public class Pair<F, S> {
+
+	private final F first;
+	private final S second;
+	
+	public Pair(F first, S second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+	public F getFirst() { return first; } 
+	public S getSecond() { return second; }
+}
