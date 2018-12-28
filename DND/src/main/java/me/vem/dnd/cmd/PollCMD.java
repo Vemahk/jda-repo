83653df01d28f50f98d55cfa53747ee0ddc92168
Vemahk <Vemahk20@gmail.com>
2018-12-28 12:34:22 -0600

    DND - Manually Closed Polls
    Added the ability to have polls be closed manually by the creator or by an admin of the guild.

--- DND/src/main/java/me/vem/dnd/cmd/PollCMD.java
+++ DND/src/main/java/me/vem/dnd/cmd/PollCMD.java
@@ -1,6 +1,7 @@
 package me.vem.dnd.cmd;
 
 import java.awt.Color;
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -53,23 +54,22 @@ public class PollCMD extends Command implements EventListener{
 		if(!super.run(event, args))
 			return false;
 		
-		if(args.length < 6)
+		if(args.length < 5)
 			return sendHelp(event.getChannel(), true);
 		
-		if(!"create".equalsIgnoreCase(args[0]))
-			return sendHelp(event.getChannel(), false);
-		
-		int seconds;
-		try {
-			seconds = Integer.parseInt(args[1]);
-		} catch (NumberFormatException e) {
-			Logger.err("Vote: could not parse number.");
-			return sendHelp(event.getChannel(), false);
+		int seconds = 0;
+		if(!"manuend".equals(args[0])) {
+			try {
+				seconds = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				Logger.err("Vote: could not parse number.");
+				return sendHelp(event.getChannel(), false);
+			}			
 		}
 		
 		Map<Emoji, String> items = new LinkedHashMap<>();
 		
-		for(int i=2;i<args.length-1;i+=2) {
+		for(int i=1;i<args.length-1;i+=2) {
 			Emoji e = null;
 			
 			if(EmojiManager.isEmoji(args[i]))
@@ -95,14 +95,17 @@ public class PollCMD extends Command implements EventListener{
 		
 		EmbedBuilder builder = new EmbedBuilder().setColor(Color.GREEN);
 		
-		builder.setDescription("This poll will expire in " + seconds + " seconds.");
+		if(seconds > 0)
+			builder.setDescription("This poll will expire in " + seconds + " seconds.");
+		else builder.setDescription("This poll is set to never expire.");
 		
 		for(Emoji emoji : items.keySet())
 			builder.addField(items.get(emoji), emoji.toString(), true);
 		
 		builder.addBlankField(false);
 		builder.addField("Please only vote once!", "Users who vote more than once will be disqualified.", false);
-		builder.setAuthor(event.getMember().getEffectiveName(), null, event.getMember().getUser().getAvatarUrl());
+		builder.setAuthor(event.getAuthor().getName() + '#' + event.getAuthor().getDiscriminator(), null, event.getMember().getUser().getAvatarUrl());
+		builder.setTimestamp(Instant.now());
 		
 		event.getMessage().delete().queue();
 		Message m = Respond.sync(event.getChannel(), builder);
@@ -110,16 +113,23 @@ public class PollCMD extends Command implements EventListener{
 			if(e.isEmote()) m.addReaction(e.getEmote()).queue();
 			else m.addReaction(e.toString()).queue();
 		
-		Poll v = new Poll(m, items);
-		activePolls.add(v);
-		
-		timer.schedule(new Task(() -> {
-			v.end();
-			activePolls.remove(v);
-		}), seconds * 1000);
+		Poll poll = new Poll(m, items).setAuthor(event.getAuthor());
+		activePolls.add(poll);
+
+		if(seconds <= 0) {
+			m.addReaction(CANCEL.toString()).queue();
+			poll.setManual();
+		} else {
+			timer.schedule(new Task(() -> {
+				poll.end();
+				activePolls.remove(poll);
+			}), seconds * 1000);
+		}
 		
 		return true;
 	}
+
+	private static final Emoji CANCEL = new Emoji("\u274C");
 	
 	@Override
 	public void onEvent(Event event) {
@@ -133,9 +143,35 @@ public class PollCMD extends Command implements EventListener{
 		if(event.getUser().getIdLong() == event.getJDA().getSelfUser().getIdLong())
 			return;
 		
+		Poll target = null;
 		for(Poll poll : activePolls)
-			if(poll.msg.getIdLong() == event.getMessageIdLong())
-				poll.add(event.getUser(), new Emoji(event.getReactionEmote()));
+			if(poll.msg.getIdLong() == event.getMessageIdLong()) {
+				target = poll;
+				break;
+			}
+
+		if(target == null)
+			return;
+		
+		Emoji emoji = new Emoji(event.getReactionEmote());
+		
+		if(emoji.equals(CANCEL)) {
+			if(!target.isManual()) return;
+			
+			if(!event.getUser().equals(target.getAuthor()) && !event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
+				
+				event.getUser().openPrivateChannel().queue(
+					(pc) -> pc.sendMessage("Only the author of this poll, or a guild administrator, can manually close this poll.").queue()
+				);
+				
+				return;
+			}
+			
+			target.end();
+			activePolls.remove(target);
+			
+		}else
+			target.add(event.getUser(), new Emoji(event.getReactionEmote()));
 	}
 	
 	public void remReaction(MessageReactionRemoveEvent event) {
@@ -155,10 +191,12 @@ public class PollCMD extends Command implements EventListener{
 	@Override
 	public String[] usages() {
 		return new String[] {
-			"`poll create <time> <:emote1:> <`desc1`> <:emote2:> <`desc2`> ... (etc)`",
+			"``poll <time> <:emote1:> <`desc1`> <:emote2:> <`desc2`> ... (etc)``",
 			" - `time` is in units of seconds.",
 			" - Creates a poll mapping emotes to certain items to vote for.",
-			" - There must be at least two items to vote for."
+			" - There must be at least two items to vote for.",
+			"``poll manuend <:emote1:> <`desc1`> <:emote2:> <`desc2`> ...``",
+			" - Creates a poll that will not time out, but must be manually stopped."
 		};
 	}
 
@@ -177,11 +215,26 @@ public class PollCMD extends Command implements EventListener{
 		private Map<Emoji, String> items;
 		private Map<User, List<Emoji>> votes;
 		
+		private User author;
+		private boolean isManual;
+		
 		public Poll(Message msg, Map<Emoji, String> items) {
 			this.msg = msg;
 			this.items = items;
 			votes = new LinkedHashMap<>();
 		}
+		
+		public Poll setAuthor(User author) {
+			this.author = author;
+			return this;
+		}
+		
+		public User getAuthor() {
+			return author;
+		}
+		
+		public void setManual() { isManual = true; }
+		public boolean isManual() { return isManual; }
 		
 		public void add(User u, Emoji e) {
 			if(!items.containsKey(e))
